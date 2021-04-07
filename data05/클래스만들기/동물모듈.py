abf0a10f825769ea3fff6c1e1f0dd55919b19276
aliciawill <aliciawill@kakao.com>
2021-04-07 11:49:13 +0900

    data05

--- data05/클래스만들기/동물모듈.py
+++ data05/클래스만들기/동물모듈.py
@@ -6,7 +6,6 @@ class Dog:
     color = None
     field = ''
 
-
     # 생성자 함수(객체생성시 자동호출되는 함수, 초기화를 담당)
     # constructor!!
     def __init__(self, color, field):
---
+++ data05/클래스만들기/매일.py
@@ -0,0 +1,31 @@
+class Day:
+    doing = None
+    time = 0
+    location = None
+
+    def __init__(self, doing, time, location):
+        self.doing = doing
+        self.time = time
+        self.location = location
+
+    def study(self):
+        return self.doing + '를 ' + str(self.time) + '시간 하다.'
+
+    def where(self):
+        return self.location + '에서 ' + self.doing + "를 하다."
+
+    def __str__(self):
+        return self.doing + ' ' + str(self.time) + ' ' + self.location
+
+if __name__ == '__main__':
+    day1 = Day('파이썬공부', 10, '강남')
+    day2 = Day('자바공부', 11, '신촌')
+    day3 = Day('db공부', 12, '종로')
+
+    print(day1)
+    print(day2)
+    print(day3)
+
+    print(day1.study())
+    where_value = day1.where()
+    print(where_value)
