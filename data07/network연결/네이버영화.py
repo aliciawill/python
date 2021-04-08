9dfd74a8fa97d4e2d11bed274fa66a21403273bd
aliciawill <aliciawill@kakao.com>
2021-04-08 15:19:56 +0900

    data07

--- data07/network연결/네이버영화.py
+++ data07/network연결/네이버영화.py
@@ -4,51 +4,53 @@ from bs4 import BeautifulSoup
 
 url = "https://movie.naver.com/movie/running/current.nhn"
 result = requests.get(url)
+#print(result.content)
 # result
 
 content = BeautifulSoup(result.content, "html.parser")
 
 # content
 
-dt_list = content.findAll("dt", {"class":"tit"})
+dt_list = content.findAll("dt", {"class": "tit"})
 # dt_list
 
-dt_list[0].find("a").text
-
-title = []
-for dt in dt_list:
-  title.append(dt.find("a").text)
-
-# title
-
-len(dt_list)
-
-score_list = content.select('dl.info_star dd div.star_t1 a span.num')
-# score_list
-
-len(score_list)
-
-scores = []
-for score in score_list:
-   scores.append(float(score.text))
-
-# scores
-
-
-
-df = pd.DataFrame({"영화이름" : title, "영화평점" : scores})
-# df
-
-print(df) #dataFrame
-print(df[10:20]) #10~19
-result = df.describe() #자세한 설명
-print(result)
-print(df.head(3)) #앞에서 3개
-print(df.info()) #데이터 타입
-print(df.index) #인덱스
-print('영화이름')
-print('---------------------')
-print(df['영화이름'])
-print('---------------------')
-print(df['영화평점'])
-print(df.sort_values(by='영화평점', ascending=False))
+print(dt_list[0])
+#
+# dt_list[0].find("a").text
+#
+# title = []
+# for dt in dt_list:
+#     title.append(dt.find("a").text)
+#
+# # title
+#
+# len(dt_list)
+#
+# score_list = content.select('dl.info_star dd div.star_t1 a span.num')
+# # score_list
+#
+# len(score_list)
+#
+# scores = []
+# for score in score_list:
+#     scores.append(float(score.text))
+#
+# # scores
+#
+#
+# df = pd.DataFrame({"영화이름": title, "영화평점": scores})
+# # df
+#
+# print(df)  # dataFrame
+# print(df[10:20])  # 10~19
+# result = df.describe()  # 자세한 설명
+# print(result)
+# print(df.head(3))  # 앞에서 3개
+# print(df.info())  # 데이터 타입
+# print(df.index)  # 인덱스
+# print('영화이름')
+# print('---------------------')
+# print(df['영화이름'])
+# print('---------------------')
+# print(df['영화평점'])
+# print(df.sort_values(by='영화평점', ascending=False))
