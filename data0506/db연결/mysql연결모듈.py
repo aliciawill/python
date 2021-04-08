861574d8d0cce372ea1a65d3dd3fa5a20d6cc25a
aliciawill <aliciawill@kakao.com>
2021-04-08 10:55:26 +0900

    data06

--- data0506/db연결/mysql연결모듈.py
+++ data0506/db연결/mysql연결모듈.py
@@ -30,6 +30,20 @@ def create2(data):
     con.commit()
     con.close()
 
+def create3(datas):
+    con = pymysql.connect(host = 'localhost', port= 3306, db='shop', user='root', password='1234')
+    print(con.get_host_info())
+
+    cur = con.cursor()
+    print(cur)
+
+    sql = 'insert into member values (%s, %s, %s, %s)'
+    result = cur.executemany(sql, datas)
+    print('처리 결과', result, '개')
+
+    con.commit()
+    con.close()
+
 def read():
     pass
 
