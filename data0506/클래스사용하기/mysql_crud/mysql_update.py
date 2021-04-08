fd4c48c15abcb2fc0cd0c57cec32f6863fc33ece
aliciawill <aliciawill@kakao.com>
2021-04-08 10:14:54 +0900

    data06

--- data0506/클래스사용하기/mysql_crud/mysql_update.py
+++ data0506/클래스사용하기/mysql_crud/mysql_update.py
@@ -1,9 +1,9 @@
 import db연결.mysql연결모듈 as db
 
-id = input('아이디를 입력하세요.')
-tel = input('전화번호를 입력하세요.')
+id = input('수정할 아이디를 입력하세요.')
+tel = input('수정할 전화번호를 입력하세요.')
 
-db.create(id, pw, name, tel)
+db.update(id, tel)
 
 
 
