d3c937b520e8a09f36c219ee0577e06d8a6479b7
aliciawill <aliciawill@kakao.com>
2021-04-08 10:13:59 +0900

    data06

--- data0506/db연결/mysql연결모듈.py
+++ data0506/db연결/mysql연결모듈.py
@@ -19,8 +19,22 @@ def create(id, pw, name, tel):
 def read():
     pass
 
-def update():
-    pass
+def update(id, tel):
+    #1. mysql과 연결
+    con = pymysql.connect(host='localhost', port=3306, db='shop', user='root', password='1234')
+    print(con.get_host_info())
+
+    #2. 스트림안의 데이터를 다룰 수 있는 부품인 cursor를 획득!
+    cur = con.cursor()
+    print(cur)
+
+    #3. sql문을 만들어서 전송함.
+    sql = "update member set tel = %s where id = %s"
+    cur.execute(sql, (tel, id)) #tuple로 넣어주어야 한다.
+
+    #4. auto-commit이 안된다. 수동으로 반영시켜야 한다.
+    con.commit()
+    con.close()
 
 def delete(id):
     con = pymysql.connect(host = 'localhost', port= 3306, db='shop', user='root', password='1234')
